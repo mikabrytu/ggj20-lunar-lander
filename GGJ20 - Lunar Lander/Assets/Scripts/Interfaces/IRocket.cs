a3235e2947a1240043fbb206334acb61fe465bd6
Mikabrytu <mikabrytu@gmail.com>
2020-02-01 15:54:45 -0300

    Add IRocket interface

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Interfaces/IRocket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20.Components
+{
+    public interface IRocket
+    {
+        void ResetPosition();
+        void ResetFuel();
+        string GetFuel();
+    }
+}
