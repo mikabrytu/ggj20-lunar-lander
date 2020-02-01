43a9fb89c7738fc433aad1ae68bc5c655688eb32
Mikabrytu <mikabrytu@gmail.com>
2020-01-31 23:05:28 -0300

    Add scripts folder structure, Singleton class, GameManager and GameState enum. Register game flow on the GameManager

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Enums/GameStateTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20
+{
+    public enum GameStateType {
+        InitialMenu,
+        Game,
+        GameOverMenu
+    }
+}
---
+++ GGJ20 - Lunar Lander/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        private GameStateType gameState;
+
+        private void Start()
+        {
+            gameState = GameStateType.InitialMenu;
+        }
+
+        public void SetupInitialMenuUI()
+        {
+
+        }
+
+        public void SetupGameUI()
+        {
+
+        }
+
+        public void SetupGameOverUI()
+        {
+            
+        }
+
+        public void StartInitialMenu()
+        {
+
+        }
+
+        public void StartGame()
+        {
+            
+        }
+
+        public void StartGameOver()
+        {
+
+        }
+
+        public void GenerateLevel()
+        {
+
+        }
+
+        public void SetupRocket()
+        {
+
+        }
+
+        public void RocketLanded(bool isSafeSpot)
+        {
+
+        }
+
+        public void UpdateScore()
+        {
+            
+        }
+
+        public void CheckScore()
+        {
+
+        }
+    }
+}
