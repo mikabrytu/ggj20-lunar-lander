43dba6ea8f9f456d86fb422784909250971abd21
Mikabrytu <mikabrytu@gmail.com>
2020-02-01 14:00:48 -0300

    Add Station prefab, model and behavior

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Components/StationComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Mikabrytu.GGJ20.Events;
+
+namespace Mikabrytu.GGJ20.Components
+{
+    public class StationComponent : MonoBehaviour
+    {
+        [SerializeField] private StationModel _model;
+        [SerializeField] private int _layerCheck;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (_model.id == 0)
+                return;
+                
+            if (collision.gameObject.layer == _layerCheck)
+                EventsManager.Raise(new OnLandOnStationEvent(_model));
+        }
+    }
+}
---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Models/StationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mikabrytu.GGJ20
+{
+    [Serializable]
+    public struct StationModel
+    {
+        public int id;
+        public bool isObjective;
+    }
+}
