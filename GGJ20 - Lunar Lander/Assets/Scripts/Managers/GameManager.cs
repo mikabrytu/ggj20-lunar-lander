ab654ec3fc4482199647b32421699b8e79be4070
Mikabrytu <mikabrytu@gmail.com>
2020-02-01 02:21:37 -0300

    Add Score Manager and Setup UIs

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mikabrytu.GGJ20
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        [SerializeField] private GameObject _initialMenuUI;
+        [SerializeField] private GameObject _gameUI;
+        [SerializeField] private GameObject _gameOverUI;
+        [SerializeField] private Text _highScoreText;
+
+        private GameStateType gameState;
+        private ScoreManager scoreManager;
+
+        private void Start()
+        {
+            scoreManager = GetComponent<ScoreManager>();
+            gameState = GameStateType.InitialMenu;
+
+            StartInitialMenu();
+        }
+
+        public void SetupInitialMenuUI()
+        {
+            _initialMenuUI.SetActive(true);
+            _gameUI.SetActive(false);
+            _gameOverUI.SetActive(false);
+
+            _highScoreText.text = $"High Score: {scoreManager.GetScore()}";
+        }
+
+        public void SetupGameUI()
+        {
+            _initialMenuUI.SetActive(false);
+            _gameUI.SetActive(true);
+            _gameOverUI.SetActive(false);
+        }
+
+        public void SetupGameOverUI()
+        {
+            _initialMenuUI.SetActive(false);
+            _gameUI.SetActive(false);
+            _gameOverUI.SetActive(true);
+        }
+
+        public void StartInitialMenu()
+        {
+            SetupInitialMenuUI();
+        }
+
+        public void StartGame()
+        {
+            SetupGameUI();
+            GenerateLevel();
+        }
+
+        public void StartGameOver()
+        {
+            SetupGameOverUI();
+        }
+
+        public void GenerateLevel()
+        {
+            Debug.Log("Generating Level...");
+        }
+
+        public void SetupRocket()
+        {
+
+        }
+
+        public void RocketLanded(bool isSafeSpot)
+        {
+
+        }
+
+        public void UpdateScore()
+        {
+            scoreManager.IncreaseScore();
+        }
+
+        public void CheckScore()
+        {
+
+        }
+    }
+}
---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20
+{
+    public class ScoreManager : Singleton<ScoreManager>
+    {
+        private const string KEY = "GGJ20-LL-score";
+        private int score;
+
+        private void Start()
+        {
+            score = PlayerPrefs.GetInt(KEY, 0);
+        }
+
+        public void IncreaseScore()
+        {
+            score++;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public void SaveScore()
+        {
+            PlayerPrefs.SetInt(KEY, score);
+        }
+    }
+}
