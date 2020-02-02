cdd21a50d2afce4fb1c3bbcc0953908eaf848272
Mikabrytu <mikabrytu@gmail.com>
2020-02-02 17:56:57 -0300

    Add animated buttons

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Components/ButtonComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Mikabrytu.GGJ20.Components
+{
+    public class ButtonComponent : SerializedMonoBehaviour
+    {
+        [SerializeField] private ButtonAnimationTypes buttonType;
+
+        private void OnEnable()
+        {
+            GetComponent<Animator>().SetInteger("Button ID", (int) buttonType);
+        }
+    }
+}
---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Enums/ButtonAnimationTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20
+{
+    public enum ButtonAnimationTypes {
+        Start,
+        Restart
+    }
+}
