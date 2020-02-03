a7a565dbddcf3c62d4f03adbcdeaced5807abb16
Mikabrytu <mikabrytu@gmail.com>
2020-02-02 21:14:41 -0300

    Wrap version 1.0

---
+++ GGJ20 - Lunar Lander/Assets/Scripts/Components/BackgroundCloudComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.GGJ20.Components
+{
+    public class BackgroundCloudComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _startPosition;
+        [SerializeField] private Transform _endPosition;
+        [SerializeField] private float _velocity;
+
+        private void Update()
+        {
+            // TODO: Extract a System of this
+            transform.Translate(Vector2.left * _velocity * Time.deltaTime);
+
+            if (transform.position.x < _endPosition.position.x)
+                transform.position = _startPosition.position;
+        }
+    }
+}
